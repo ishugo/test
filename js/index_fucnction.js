dd79c302ffdbaaed1002391e349759d7b03e91e7
Hugo <pls.hugo@gmail.com>
2018-04-01 17:30:19 +0800

    改

--- js/index_fucnction.js
+++ js/index_fucnction.js
@@ -11,6 +11,26 @@ $(function(){
                  //if(msg==''){location.reload();}else{alert(msg);}
             });
     })
+
+    $(".b02").click(function(){           
+        var http = new XMLHttpRequest();
+        var url = "http://hugo.qov.tw/github/function.php";
+        var params = "opt=1";
+        http.open("POST", url, true);
+
+        http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+        http.onreadystatechange = function() {
+            if(http.readyState == 4 && http.status == 200) {
+                alert(http.responseText);
+            }
+        }
+        
+        http.send(params);
+    })
+
+
+
 //	console.log(); //debug
 //	alert(); //debug
 });
