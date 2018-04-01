bd253bb6bfba294be6b224649519a8df9e54c6e2
Hugo <pls.hugo@gmail.com>
2018-04-01 16:59:09 +0800

    改

--- js/index_fucnction.js
+++ js/index_fucnction.js
@@ -1,11 +1,15 @@
 $(function(){
-
-
-
-
-
     $(".b01").click(function(){
-        $("#editor_m").html("Hello Hugo");
+        $.ajax('http://hugo.qov.tw/github/function.php',{  
+            type:'POST'
+            ,data:{	
+                opt:1
+            }
+            }).done(function(msg){
+                $("#editor_m").html(msg);
+                 //$(".alert_java").html(msg);
+                 //if(msg==''){location.reload();}else{alert(msg);}
+            });
     })
 //	console.log(); //debug
 //	alert(); //debug
