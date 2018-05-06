e8a86e5f2e96d3a0a6411a430590146bb9e2f786
Hugo <pls.hugo@gmail.com>
2018-05-06 17:47:50 +0800

    @

--- js/index_fucnction.js
+++ js/index_fucnction.js
@@ -2,25 +2,34 @@ $(function(){
     $(".b01").click(function(){ 
         //http://ook.dynu.net:9071/github/function.php
         //https://hugo.qov.tw/github/function.php
-        $.ajax('http://ook.dynu.net:9071/github/function.php',{  
-            type:'POST'
-            ,data:{	
+        // $.ajax('http://ook.dynu.net:9071/github/function.php',{  
+        //     type:'POST'
+        //     ,data:{	
+        //         opt:1
+        //     }
+        //     }).done(function(msg){
+        //         alert(msg);
+        //         $("#editor_m").html(msg);
+        //          //$(".alert_java").html(msg);
+        //          //if(msg==''){location.reload();}else{alert(msg);}
+        // });
+        axios({
+            method: 'post',
+            url: 'http://ook.dynu.net:9071/github/function.php',
+            data: {
                 opt:1
-            }
-            }).done(function(msg){
+            }.done(function(msg){
                 alert(msg);
                 $("#editor_m").html(msg);
-                 //$(".alert_java").html(msg);
-                 //if(msg==''){location.reload();}else{alert(msg);}
-            });
+            )
+        });
+
     })
 
     $(".b02").click(function(){           
         var http = new XMLHttpRequest();
         var url = "http://ook.dynu.net:9071/github/function.php";
         var params = "opt=1";
-        http.open("POST", url, true);
-
         http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
         http.onreadystatechange = function() {
@@ -28,7 +37,7 @@ $(function(){
                 alert(http.responseText);
             }
         }
-
+        http.open("POST", url, true);
         http.send(params);
     })
 
