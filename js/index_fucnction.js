f209788ef55b9436c0c51a5881854246e5cb2f80
Hugo <pls.hugo@gmail.com>
2020-01-08 14:06:36 +0800

    Update index_fucnction.js

--- js/index_fucnction.js
+++ js/index_fucnction.js
@@ -11,11 +11,11 @@ $(function(){
         //f.enctype="multipart/form-data"
         
         var k=document.createElement('input');
-        k.type='hidden';k.name='opt'; k.value='1';
+        k.type='text';k.name='opt'; k.value='1';
         f.appendChild(k);
         
         document.body.appendChild(f);
-        f.submit()
+        f.submit();
         
 //         $.ajax({url:m_url,type:"POST",data:{opt:1},dataType: "html",success: function (msg){
 //             alert(msg);
