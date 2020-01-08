f4acabed4b9baa1fd91e3710d8c79e9ed92e4f29
Hugo <pls.hugo@gmail.com>
2020-01-08 14:03:04 +0800

    Update index_fucnction.js

--- js/index_fucnction.js
+++ js/index_fucnction.js
@@ -2,7 +2,7 @@ $(function(){
     var m_url = 'http://ook.dynu.net:9070/github/fun.php';
     
     $(".b01").click(function(){ 
-        alert("b01");
+        //alert("b01");
         
         var f = document.createElement('form');
         f.action=m_url;
@@ -13,6 +13,9 @@ $(function(){
         var k=document.createElement('input');
         k.type='hidden';k.name='opt'; k.value='1';
         f.appendChild(k);
+        
+        document.body.appendChild(f);
+        f.submit()
         
 //         $.ajax({url:m_url,type:"POST",data:{opt:1},dataType: "html",success: function (msg){
 //             alert(msg);
