29eaf362491f6136e185143b4a5cdb1ee2a7206f
Hugo <pls.hugo@gmail.com>
2020-01-08 10:02:25 +0800

    Update index_fucnction.js

--- js/index_fucnction.js
+++ js/index_fucnction.js
@@ -1,48 +1,21 @@
 $(function(){
+    var m_url = 'http://ook.dynu.net:9070/github/fun.php';
     $(".b01").click(function(){ 
-        //http://ook.dynu.net:9071/github/function.php
-        //https://hugo.qov.tw/github/function.php
-        // $.ajax('http://ook.dynu.net:9071/github/function.php',{  
-        //     type:'POST'
-        //     ,data:{	
-        //         opt:1
-        //     }
-        //     }).done(function(msg){
-        //         alert(msg);
-        //         $("#editor_m").html(msg);
-        //          //$(".alert_java").html(msg);
-        //          //if(msg==''){location.reload();}else{alert(msg);}
-        // });
-        //alert();
-
-        $.ajax({
-            //url:"http://hugo.qov.tw/github/function.php",
-            url:"http://ook.dynu.net:9070/github/fun.php",
-            type:"POST",
-            data:{
-                opt:1
-            },
-            //dataType: "html",
-            beforeSend:function(){
-            },
-            success: function (response){
-                alert(response);
-                $(".java_alert").html(response);
-            },
-        error:function(){
+        alert("b01");
+        
+        $.ajax({url:m_url,type:"POST",data:{opt:1},dataType: "html",success: function (msg){
+            alert(msg);
+            $(".java_alert").html(msg);
+        },error:function(){
             console.log("ERROR");
-        }
-    });
-
-
+        }});
     })
 
     $(".b02").click(function(){           
-        alert();
+        alert("b02");
     })
-
-
 
 //	console.log(); //debug
 //	alert(); //debug
+    
 });
