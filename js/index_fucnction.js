a2974b1a17f219e9faddccc6d8f0b920eaef0029
Hugo <pls.hugo@gmail.com>
2020-01-08 10:22:47 +0800

    Update index_fucnction.js

--- js/index_fucnction.js
+++ js/index_fucnction.js
@@ -1,15 +1,19 @@
 $(function(){
     var m_url = 'https://ook.dynu.net:9070/github/fun.php';
+    
     $(".b01").click(function(){ 
         alert("b01");
         
         $.ajax({url:m_url,type:"POST",data:{opt:1},dataType: "html",success: function (msg){
             alert(msg);
             $(".java_alert").html(msg);
-        },error:function(xhr, ajaxOptions, thrownError){
-            console.log(xhr.status);
-            console.log(xhr.responseText);
-            alert(thrownError);
+        },error:function(jqXHR, textStatus, errorThrown){
+            console.log("responseText:"+jqXHR.responseText);
+            console.log("status:"+jqXHR.status);
+            console.log("readyState:"+jqXHR.readyState);
+            console.log("statusText:"+jqXHR.statusText);
+            console.log("textStatus:"+textStatus);
+            console.log("errorThrown:"+errorThrown);
         }});
     })
 
