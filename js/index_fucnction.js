5c5fe2ef2c44de94c60735aad1d36d7dfd1383d1
Hugo <pls.hugo@gmail.com>
2020-03-27 16:14:33 +0800

    Update index_fucnction.js

--- js/index_fucnction.js
+++ js/index_fucnction.js
@@ -1,8 +1,11 @@
 $(function(){
+
+sydb();
+	
+function sydb(){
     var m_url = 'https://ook.dynu.net:67/github/fun.php';
-    
     $.ajax({url:m_url,type:"POST",data:{opt:1},dataType: "html",success: function (msg){
-        $(".java_alert").html(msg);
+	$(".java_alert").html(msg);
 	$("#example").DataTable({ //各式表格列表
 		//"scrollX": true,
 		//"sDom": 'Rfrtlip',
@@ -38,26 +41,16 @@ $(function(){
 	// 			alert();
 	// 		}},	
 	// 	]
-		
-    	});	
+
+	});	
     },error:function(jqXHR, textStatus, errorThrown){
 
     }});
+}
+
 
     $(".b01").click(function(){       
-        
-        $.ajax({url:m_url,type:"POST",data:{opt:1},dataType: "html",success: function (msg){
-            //alert(msg);
-            $(".java_alert").html(msg);
-        },error:function(jqXHR, textStatus, errorThrown){
-            console.log("responseText:"+jqXHR.responseText);
-            console.log("status:"+jqXHR.status);
-            console.log("readyState:"+jqXHR.readyState);
-            console.log("statusText:"+jqXHR.statusText);
-            console.log("textStatus:"+textStatus);
-            console.log("errorThrown:"+errorThrown);
-        }});
-        
+	sydb(); 
     })
 
     $(".b02").click(function(){            
