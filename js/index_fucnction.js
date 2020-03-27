a243d96a46c96584f2e09f0de1068ef96981c52c
Hugo <pls.hugo@gmail.com>
2020-03-27 15:23:22 +0800

    Update index_fucnction.js

--- js/index_fucnction.js
+++ js/index_fucnction.js
@@ -5,31 +5,31 @@ $(function(){
         //alert("b01");
         
         //傳遞 POST 給 ook.dynu.net --------------------
-        var f = document.createElement('form');
-        f.action=m_url;
-        f.method='POST';
-        f.target='_blank';
-        f.enctype="multipart/form-data"
+//         var f = document.createElement('form');
+//         f.action=m_url;
+//         f.method='POST';
+//         f.target='_blank';
+//         f.enctype="multipart/form-data"
         
-        var k=document.createElement('input');
-        k.type='text';k.name='opt'; k.value='1';
-        f.appendChild(k);
+//         var k=document.createElement('input');
+//         k.type='text';k.name='opt'; k.value='1';
+//         f.appendChild(k);
         
-        document.body.appendChild(f);
-        f.submit();
+//         document.body.appendChild(f);
+//         f.submit();
         // ------------------------------------------
         
-//         $.ajax({url:m_url,type:"POST",data:{opt:1},dataType: "html",success: function (msg){
-//             alert(msg);
-//             $(".java_alert").html(msg);
-//         },error:function(jqXHR, textStatus, errorThrown){
-//             console.log("responseText:"+jqXHR.responseText);
-//             console.log("status:"+jqXHR.status);
-//             console.log("readyState:"+jqXHR.readyState);
-//             console.log("statusText:"+jqXHR.statusText);
-//             console.log("textStatus:"+textStatus);
-//             console.log("errorThrown:"+errorThrown);
-//         }});
+        $.ajax({url:m_url,type:"POST",data:{opt:1},dataType: "html",success: function (msg){
+            alert(msg);
+            $(".java_alert").html(msg);
+        },error:function(jqXHR, textStatus, errorThrown){
+            console.log("responseText:"+jqXHR.responseText);
+            console.log("status:"+jqXHR.status);
+            console.log("readyState:"+jqXHR.readyState);
+            console.log("statusText:"+jqXHR.statusText);
+            console.log("textStatus:"+textStatus);
+            console.log("errorThrown:"+errorThrown);
+        }});
         
     })
 
