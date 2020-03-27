277d8fd2206382b2223ffd092f87820604ae1bed
Hugo <pls.hugo@gmail.com>
2020-03-27 15:41:38 +0800

    Update index_fucnction.js

--- js/index_fucnction.js
+++ js/index_fucnction.js
@@ -1,23 +1,13 @@
 $(function(){
     var m_url = 'https://ook.dynu.net:67/github/fun.php';
     
-    $(".b01").click(function(){ 
-        //alert("b01");
-        
-        //傳遞 POST 給 ook.dynu.net --------------------
-//         var f = document.createElement('form');
-//         f.action=m_url;
-//         f.method='POST';
-//         f.target='_blank';
-//         f.enctype="multipart/form-data"
-        
-//         var k=document.createElement('input');
-//         k.type='text';k.name='opt'; k.value='1';
-//         f.appendChild(k);
-        
-//         document.body.appendChild(f);
-//         f.submit();
-        // ------------------------------------------
+    $.ajax({url:m_url,type:"POST",data:{opt:1},dataType: "html",success: function (msg){
+        $(".java_alert").html(msg);
+    },error:function(jqXHR, textStatus, errorThrown){
+
+    }});
+
+    $(".b01").click(function(){       
         
         $.ajax({url:m_url,type:"POST",data:{opt:1},dataType: "html",success: function (msg){
             //alert(msg);
