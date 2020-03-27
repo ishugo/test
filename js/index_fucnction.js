ec8d138aea94c7de051d33d13a3553e05908f5e8
Hugo <pls.hugo@gmail.com>
2020-03-27 15:49:29 +0800

    Update index_fucnction.js

--- js/index_fucnction.js
+++ js/index_fucnction.js
@@ -1,3 +1,43 @@
+function dataTable_main(name){ //指定 dataTable name,html
+    $("#"+name).DataTable({ //各式表格列表
+        //"scrollX": true,
+		//"sDom": 'Rfrtlip',
+		altEditor: true,
+        "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+        "order": [[ 1, 'DESC' ]],
+        "language": {
+            "lengthMenu": "每頁 _MENU_ 筆",
+            "zeroRecords": "抱歉，沒有找到！！！",
+            "info": "總資料 _MAX_ 筆，_START_ 筆至 _END_ 筆，_PAGE_ 至 _PAGES_ 頁",
+            "infoEmpty": "沒有可用的記錄",
+            "infoFiltered": "(搜尋 _TOTAL_ 筆，總資料 _MAX_ 筆)",
+            "loadingRecords": "載入中...",
+            "processing":     "處理中...",
+            "search":"搜尋",
+            "paginate": {
+                "first":      "第一頁",
+                "last":       "最後一頁",
+                "next":       "下一頁",
+                "previous":   "上一頁"
+            },
+            "aria": {
+                "sortAscending":  ": 按升序對列進行排序",
+                "sortDescending": ": 按降序對列進行排序"
+            }
+        },
+        "dom" : 'lBfrtip',
+        destroy: true,
+		searching: true,
+
+// 		buttons: [
+// 			{text: '新增 - 通用必要軟體項',action: function ( e, dt, node, config ) {
+// 			    alert();
+// 			}},	
+// 		]
+		
+		});
+}
+
 $(function(){
     var m_url = 'https://ook.dynu.net:67/github/fun.php';
     
