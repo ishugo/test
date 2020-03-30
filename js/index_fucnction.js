52e17f7e23e11a1984215b03820c5de72311cc84
Hugo <pls.hugo@gmail.com>
2020-03-30 09:18:37 +0800

    Update index_fucnction.js

--- js/index_fucnction.js
+++ js/index_fucnction.js
@@ -1,3 +1,57 @@
+function dialog_main(name,msg,title,x,y,width,height){ //指定 dialog name,html,標題,x,y (可設 "auto")
+    $('<div class="'+name+'"></div>').appendTo('body').html(msg).dialog({title: title,resizable: false,
+      modal: true, zIndex: 10000,autoOpen: true, width: width ,height: height,position: { my: x, at: y },
+      show: {effect: "slide"},hide: {effect: "clip"},
+       close: function (event, ui) {$(this).remove();}
+    });
+
+    $(".ui-widget").css({
+        "font-family": "微軟正黑體,Verdana,Arial,sans-serif",
+        "font-size": "1.0rem"
+    });
+}
+
+function dataTable_main(name){ //指定 dataTable name,html
+    $("#"+name).DataTable({ //各式表格列表
+        //"scrollX": true,
+		//"sDom": 'Rfrtlip',
+		altEditor: true,
+        "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+        "order": [[ 1, 'DESC' ]],
+        "language": {
+            "lengthMenu": "每頁 _MENU_ 筆",
+            "zeroRecords": "抱歉，沒有找到！！！",
+            "info": "總資料 _MAX_ 筆，_START_ 筆至 _END_ 筆，_PAGE_ 至 _PAGES_ 頁",
+            "infoEmpty": "沒有可用的記錄",
+            "infoFiltered": "(搜尋 _TOTAL_ 筆，總資料 _MAX_ 筆)",
+            "loadingRecords": "載入中...",
+            "processing":     "處理中...",
+            "search":"搜尋",
+            "paginate": {
+                "first":      "第一頁",
+                "last":       "最後一頁",
+                "next":       "下一頁",
+                "previous":   "上一頁"
+            },
+            "aria": {
+                "sortAscending":  ": 按升序對列進行排序",
+                "sortDescending": ": 按降序對列進行排序"
+            }
+        },
+        "dom" : 'lBfrtip',
+        destroy: true,
+		searching: true,
+
+// 		buttons: [
+// 			{text: '新增 - 通用必要軟體項',action: function ( e, dt, node, config ) {
+// 			    alert();
+// 			}},	
+// 		]
+		
+		});
+}
+
+
 $(function(){
 
 sydb('1');
@@ -6,43 +60,44 @@ function sydb(opt){
     var m_url = 'https://ook.dynu.net:67/github/fun.php';
     $.ajax({url:m_url,type:"POST",data:{opt:opt},dataType: "html",success: function (msg){
 	$(".java_alert").html(msg);
-	$("#example").DataTable({ //各式表格列表
-		//"scrollX": true,
-		//"sDom": 'Rfrtlip',
-		//altEditor: true,
-		lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "All"]],
-		order: [[ 1, 'DESC' ]],
-		language: {
-		    "lengthMenu": "每頁 _MENU_ 筆",
-		    "zeroRecords": "抱歉，沒有找到！！！",
-		    "info": "總資料 _MAX_ 筆，_START_ 筆至 _END_ 筆，_PAGE_ 至 _PAGES_ 頁",
-		    "infoEmpty": "沒有可用的記錄",
-		    "infoFiltered": "(搜尋 _TOTAL_ 筆，總資料 _MAX_ 筆)",
-		    "loadingRecords": "載入中...",
-		    "processing":     "處理中...",
-		    "search":"搜尋",
-		    "paginate": {
-			"first":      "第一頁",
-			"last":       "最後一頁",
-			"next":       "下一頁",
-			"previous":   "上一頁"
-		    },
-		    "aria": {
-			"sortAscending":  ": 按升序對列進行排序",
-			"sortDescending": ": 按降序對列進行排序"
-		    }
-		},
-		dom: 'lBfrtip',
-		destroy: true,
-		searching: true
+	dataTable_main("example");
+// 	$("#example").DataTable({ //各式表格列表
+// 		//"scrollX": true,
+// 		//"sDom": 'Rfrtlip',
+// 		//altEditor: true,
+// 		lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "All"]],
+// 		order: [[ 1, 'DESC' ]],
+// 		language: {
+// 		    "lengthMenu": "每頁 _MENU_ 筆",
+// 		    "zeroRecords": "抱歉，沒有找到！！！",
+// 		    "info": "總資料 _MAX_ 筆，_START_ 筆至 _END_ 筆，_PAGE_ 至 _PAGES_ 頁",
+// 		    "infoEmpty": "沒有可用的記錄",
+// 		    "infoFiltered": "(搜尋 _TOTAL_ 筆，總資料 _MAX_ 筆)",
+// 		    "loadingRecords": "載入中...",
+// 		    "processing":     "處理中...",
+// 		    "search":"搜尋",
+// 		    "paginate": {
+// 			"first":      "第一頁",
+// 			"last":       "最後一頁",
+// 			"next":       "下一頁",
+// 			"previous":   "上一頁"
+// 		    },
+// 		    "aria": {
+// 			"sortAscending":  ": 按升序對列進行排序",
+// 			"sortDescending": ": 按降序對列進行排序"
+// 		    }
+// 		},
+// 		dom: 'lBfrtip',
+// 		destroy: true,
+// 		searching: true
 
-	// 	buttons: [
-	// 		{text: '新增 - 通用必要軟體項',action: function ( e, dt, node, config ) {
-	// 			alert();
-	// 		}},	
-	// 	]
+// 	// 	buttons: [
+// 	// 		{text: '新增 - 通用必要軟體項',action: function ( e, dt, node, config ) {
+// 	// 			alert();
+// 	// 		}},	
+// 	// 	]
 
-	});	
+// 	});	
     },error:function(jqXHR, textStatus, errorThrown){
 
     }});
