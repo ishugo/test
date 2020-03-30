ab251647a1c4d17d65e3b825a5ff68d1c86f5fa8
Hugo <pls.hugo@gmail.com>
2020-03-30 10:31:04 +0800

    Update index_fucnction.js

--- js/index_fucnction.js
+++ js/index_fucnction.js
@@ -149,6 +149,9 @@ sydb('1');
 			    }
 			]
 		    } );
+		    table.buttons( 0, null ).container().prependTo(
+			table.table().container()
+		    );
 	    },error:function(jqXHR, textStatus, errorThrown){
 
 	    }});
