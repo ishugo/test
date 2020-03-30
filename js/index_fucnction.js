881e33c0afc9adb0878e90804581c63db4e0e8d6
Hugo <pls.hugo@gmail.com>
2020-03-30 09:29:57 +0800

    Update index_fucnction.js

--- js/index_fucnction.js
+++ js/index_fucnction.js
@@ -1,3 +1,47 @@
+function input_small(name, title,type,m_val){ //輸入欄位 small name, title,type,資料
+    var input_m = '<div class="input-group input-group-sm mb-3">'+
+    '<div class="input-group-prepend"><span class="input-group-text" id="inputGroup-sizing-sm">'+title+
+    '</span></div><input type="'+type+'" name="'+name+'" value="'+m_val+'" class="w_db form-control my_input" aria-label="Small" aria-describedby="inputGroup-sizing-sm"></div>';
+    return input_m;
+}
+
+function input_m(name, title,type,m_val){ //輸入欄位 small name, title,type
+    var input_m = '<div class="input-group mb-3"><div class="input-group-prepend"><span class="input-group-text" id="inputGroup-sizing-default">'+title+'</span></div><input type="'+type+'" name="'+name+'" value="'+m_val+'" class="w_db form-control my_input" aria-label="Default" aria-describedby="inputGroup-sizing-default"></div>';
+    return input_m;
+}
+
+function abutton_ui(name, title, src, target){ //輸入按鈕 id, title、src、target
+    var button_m = '<a href="'+src+'" id="'+name+'" class="button_m ui-button ui-widget ui-corner-all" target="'+target+'" role="button">'+title+'</a>';
+    return button_m;
+}
+
+function textarea_m(name, title, row){ //輸入編輯欄位 name, title, row
+    var textarea_q = '<div class="mb-3"><div class="form-group"><label for="textarea_m">'+title+'</label><textarea class="w_db form-control" name="'+name+'" id="textarea_m" rows="'+row+'"></textarea></div></div>';
+    return textarea_q;
+}
+
+function radio_m(name, title, value_m){ //輸入Radio name, title, value_m
+    var radio_q = '<div class="input-group mb-3"><div class="form-check"><input type="radio" class="w_db form-check-input radio_m" name="'+name+'" value="'+value_m+'"><label class="form-check-label" for="radio_m">'+title+'</label></div></div>';
+    return radio_q;
+}
+
+function select_m(name, select_a,value_m){ //輸入Select name, select_a,value_m
+    var sel_m = select_a.split(",");
+    var val_m = value_m.split(",");
+    var select_q = '<div class="input-group mb-3"><select name="'+name+'" class="w_db browser-default custom-select">';
+        select_q += '<option selected>請選擇</option>';
+        for($i=0;$i<sel_m.length;$i++){
+            select_q += '<option value="'+$.trim(val_m[$i])+'">'+$.trim(sel_m[$i])+'</option>';        
+        }
+        select_q += '</select></div>';
+    return select_q;
+}
+
+function checkbox_m(name,title,value_m){ //輸入CheckBox name,title,value_m
+    var checkbox_q = '<div class="input-group mb-3"><div class="form-check"><input type="checkbox" name="'+name+'" class="w_db form-check-input checkbox_m" value="'+value_m+'"><label class="form-check-label" for="checkbox_m">'+title+'</label></div></div>';
+    return checkbox_q;
+}
+
 function dialog_main(name,msg,title,x,y,width,height){ //指定 dialog name,html,標題,x,y (可設 "auto")
     $('<div class="'+name+'"></div>').appendTo('body').html(msg).dialog({title: title,resizable: false,
       modal: true, zIndex: 10000,autoOpen: true, width: width ,height: height,position: { my: x, at: y },
@@ -81,6 +125,25 @@ sydb('1');
 		'<div class="col-md-6"><div class="main2"></div></div></div>';
 		
 		dialog_main("dog",html_m,"測試123","center-200px","top+300px","auto","auto"); 
+		
+		$(".main1").append(input_m("in01","名稱","text",""));  
+		//輸入欄位 small name, title,type
+		$(".main1").append(input_m("in02","密碼","password","")); 
+		//輸入Select name, select_a,value_m
+		$(".main1").append(select_m("sel01","ABC, DEF","aaa, bbb")); 
+		//輸入Radio name, title, value_m
+		$(".main1").append('<div class="form-inline">'+radio_m("radio1","男","男")+radio_m("radio1","女","女")+'</div>'); 
+		//輸入按鈕 id, title、src、target
+		$(".main1").append('<span>'+abutton_ui("but1","測試1","http://tw.yahoo.com","_blank")+'</span>'); 
+		$(".main1").append('<span>'+abutton_ui("but2","測試2","https://www.spexcourier.com/","_blank")+'</span>'); 
+
+
+		//輸入編輯欄位 name, title, row
+		$(".main2").append(textarea_m("textarea_w","留言","5"));
+		//輸入CheckBox name,title,value_m
+		$(".main2").append('<div class="form-inline">'+checkbox_m("ck1","台北市","北市")+checkbox_m("ck2","台南市","南市")+'</div>');
+		//輸入按鈕 id, title、src、target
+		$(".main2").append('<span class="d-flex flex-row-reverse">'+abutton_ui("but6","測試 console.log","javascript:;","")+'</span>'); 
 		
 	})
 
