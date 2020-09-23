70a93e37786acbeb780b1ce4c87e5781c99298f5
Hugo <pls.hugo@gmail.com>
2020-09-23 16:54:15 +0800

    修 8

--- js/index_fucnction.js
+++ js/index_fucnction.js
@@ -230,7 +230,14 @@ $(function(){
 		var m_url = 'https://ook.dynu.net:67/github/fun.php';
 		var tid = $(this).attr("t");
 		$.post( m_url,{opt:5,tid:tid}, function( msg ) {
-			console.log(msg); //debug
+			//console.log(msg); //debug
+			var res = JSON.parse(msg); //解析JSON
+			console.log(res); //debug
+			dialog_main("dog1",'<div class="main1"></div>',"新增","","","210","210");
+			$(".main1").append(input_m("in01","try1","text","")); 
+			$(".main1").append(input_m("in02","try2","text","")); 
+			$(".main1").append('<br><span class="d-flex flex-row-reverse">'+abutton_ui("send_e","送出","javascript:;","")+'</span>'); 
+			$("#send_e").css("display","block");
 			//alert("新增成功！");
 			//sydb('1');
 			//$(".dog1").dialog("close");
