77799f2d1355b84757f75d103ae23d41daca7d35
ishugo <pls.hugo@gmail.com>
2020-09-26 09:45:41 +0800

    修 1

--- js/index_fucnction.js
+++ js/index_fucnction.js
@@ -99,40 +99,37 @@ function del_all(){ //全選 // 不全選
 	}
 }
 
+function sydb(opt){
+	var m_url = 'https://ook.dynu.net:67/github/fun.php';
+	
+	$.ajax({url:m_url,type:"POST",data:{opt:opt},dataType: "html",success: function (msg){	
 
-$(function(){
-
-	sydb('1');
-
-	function sydb(opt){
-	    var m_url = 'https://ook.dynu.net:67/github/fun.php';
-	    $.ajax({url:m_url,type:"POST",data:{opt:opt},dataType: "html",success: function (msg){		    
 		$(".java_alert").html(msg);
-		    
+			
 		var table = $('#example').DataTable({ //各式表格列表 
 		"lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
 		"order": [[ 1, 'DESC' ]],
 		"language": {
-		    "lengthMenu": "每頁 _MENU_ 筆",
-		    "zeroRecords": "抱歉，沒有找到！！！",
-		    "info": "總資料 _MAX_ 筆，_START_ 筆至 _END_ 筆，_PAGE_ 至 _PAGES_ 頁",
-		    "infoEmpty": "沒有可用的記錄",
-		    "infoFiltered": "(搜尋 _TOTAL_ 筆，總資料 _MAX_ 筆)",
-		    "loadingRecords": "載入中...",
-		    "processing":     "處理中...",
-		    "search":"搜尋",
-		    "paginate": {
+			"lengthMenu": "每頁 _MENU_ 筆",
+			"zeroRecords": "抱歉，沒有找到！！！",
+			"info": "總資料 _MAX_ 筆，_START_ 筆至 _END_ 筆，_PAGE_ 至 _PAGES_ 頁",
+			"infoEmpty": "沒有可用的記錄",
+			"infoFiltered": "(搜尋 _TOTAL_ 筆，總資料 _MAX_ 筆)",
+			"loadingRecords": "載入中...",
+			"processing":     "處理中...",
+			"search":"搜尋",
+			"paginate": {
 			"first":      "第一頁",
 			"last":       "最後一頁",
 			"next":       "下一頁",
 			"previous":   "上一頁"
-		    },
-		    "aria": {
+			},
+			"aria": {
 			"sortAscending":  ": 按升序對列進行排序",
 			"sortDescending": ": 按降序對列進行排序"
-		    }
+			}
 		}});
-		    		    
+						
 		switch(opt) {  
 			case "1": //新增、刪除			
 				new $.fn.dataTable.Buttons( table, {
@@ -160,19 +157,23 @@ $(function(){
 				});
 				
 				table.buttons( 0, null ).container().prependTo(
-				    table.table().container()
+					table.table().container()
 				);
 			break;
 			case "2":
 			break;
 		}
 
-		    
-	    },error:function(jqXHR, textStatus, errorThrown){
+	},error:function(jqXHR, textStatus, errorThrown){
 
-	    }});
-	}
-	
+	}});
+}
+
+
+$(function(){
+
+	sydb('1');
+
 	$(document).on("click","#send_a",function(){ //新增
 		var m_url = 'https://ook.dynu.net:67/github/fun.php';
 		var try1 = $("input[name='in01']").val();
@@ -188,11 +189,11 @@ $(function(){
 		
 	});
 
-	$(".b01").click(function(){ //查尋測試       
+	$(document).on("click",".b01",function(){ //查尋測試  
 		sydb('1');
 	})
 
-	$(".b02").click(function(){ //查尋複製網站           
+	$(document).on("click",".b02",function(){  //查尋複製網站          
 		sydb('2');
 	})
 
@@ -259,8 +260,6 @@ $(function(){
 		
 	});
 		
-	
-
 //	console.log(); //debug
 //	alert(); //debug
     
